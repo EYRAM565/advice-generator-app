01d20fcb703a9a7317ca2a41e3e89b50d6d26f33
EYRAM565 <raymond.tublu@yahoo.com>
2022-07-25 08:55:34 +0100

    update

--- js/style.js
+++ js/style.js
@@ -1,14 +1,14 @@
-document.querySelector('#dice').addEventListener('click', ()=> {
+document.querySelector('button').addEventListener('click', ()=> {
     console.log('testing');
     fetch('	https://api.adviceslip.com/advice')
      .then(res => res.json())
      .then(data => {
         const slip = data.slip
         document.querySelector('#header').innerText =  ` ${"ADVICE #"}` + slip.id;
-        document.querySelector('p').innerText = `${slip.advice}`;
+        document.querySelector('.text').innerText = `${slip.advice}`;
         console.log(data);
     })
     .catch(err => {
-        console.log(`error $ {error}`);
+        console.log(`error ${err}`);
     })
 })
